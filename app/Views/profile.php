62b641e8b409572ca26aac6705b7c1b140ee362e
Muhammad Al-Farrel Elmaretza <alfarrelelmaretza@gmail.com>
2023-09-12 17:09:29 +0700

    initial commit

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <style>
+        .container{
+            margin-top: 200px;
+            
+        }
+    </style>
 </head>
 <center>
 <body>
-<img src="<?= base_url('faphoto.jpg'); ?>" alt="Profile" height = "240" width = "220" margin= "auto">
+    <div class="container" display = "flex" align-item = "center" justify-content = "center">
+<img src="<?= base_url('faphoto.jpg'); ?>" alt="Profile" height = "240" width = "220"  >
     <table>
 <tr>
     <td>Nama</td>
@@ -28,6 +35,7 @@
 </tr>
 </table>
 
+</div>
 </body>
 </center>
 </html>
