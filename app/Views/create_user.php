1167f6b0b5c86ffa9213f6333187e95957e10e47
Muhammad Al-Farrel Elmaretza <alfarrelelmaretza@gmail.com>
2023-09-29 08:19:32 +0700

    update

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -11,13 +11,36 @@
     <div class="container2">
     <form action="<?=base_url('/user/store')?>" method = 'post'>
         <label for="nama">Nama</label>
-        <input type="text" name="nama"><br> 
-        
-        <label for="kelas">Kelas</label>
-        <input type="text" name="kelas"><br> 
+        <input type="text" name="nama"<?= session('validation') && session('validation')->hasError('nama') ? 'is-invalid' : '' ?>" id="floatingName" placeholder="Nama" name="nama" value="<?= old('nama') ?>">><br> 
+        <?php if (session('validation') && session('validation')->hasError('nama')) : ?>
+          <div class="invalid-feedback">
+            <?= session('validation')->getError('nama'); ?>
+          </div>
+        <?php endif; ?>
 
         <label for="npm">NPM</label>
-        <input type="text" name="npm"><br> 
+        <input type="text" name="npm" <?= session('validation') ? 'is-invalid' : '' ?>" id="floatingNpm" placeholder="NPM" name="npm" value="<?= old('npm') ?>"><br> 
+        <?php if (session('validation') && session('validation')->hasError('npm')) : ?>
+          <div class="invalid-feedback">
+            <?= session('validation')->getError('npm'); ?>
+          </div>
+        <?php endif; ?>
+
+        <tr>
+            <td>Kelas</td>
+            <td>:</td>
+            <td>
+                <select name="kelas" id="kelas">
+                    <?php foreach($kelas as $item):?>
+                        <option value="<?=$item['id']?>">
+                            <?=$item['nama_kelas']?>
+                        </option>
+                    <?php endforeach;?>
+                </select>
+            </td>
+        </tr>
+
+
 
         <input type="Submit">
     </form>
